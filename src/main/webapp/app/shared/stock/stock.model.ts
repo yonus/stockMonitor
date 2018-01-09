1420dc3e741a91ad117ede8b58a92adbcd730798
yunus <yonuskula@gmail.com>
2018-01-10 01:11:29 +0300

    added files

---
+++ src/main/webapp/app/shared/stock/stock.model.ts
@@ -0,0 +1,12 @@
+export class StockModel {
+
+    public id?:any;
+    public code:String;
+    public name:String;
+    public createdDate?: Date;
+    public lastModifiedBy?: string;
+    public lastModifiedDate?: Date;
+
+
+
+}
---
+++ src/main/webapp/app/shared/stock/stock.service.ts
@@ -0,0 +1,53 @@
+import { Injectable } from '@angular/core';
+import {Http, Response} from "@angular/http";
+import { SERVER_API_URL } from '../../app.constants';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/catch';
+import 'rxjs/add/observable/throw';
+
+import {StockModel} from "./stock.model";
+import {ResponseWrapper} from "../model/response-wrapper.model";
+
+@Injectable()
+export class StockService {
+    private resourceUrl = SERVER_API_URL + 'api/stock';
+
+
+    constructor(private http:Http) {
+
+     }
+
+
+     getStocks():Observable<StockModel[]>{
+        return this.http.get(this.resourceUrl+"/allStocks").map(res => res.json());
+     }
+
+     getStockByCode(code:string):Observable<StockModel>{
+        return this.http.get(this.resourceUrl+"/"+code).map(res => res.json());
+
+     }
+
+     create(stock:StockModel):Observable<ResponseWrapper>{
+        stock.code = stock.code.toUpperCase();
+        stock.name = stock.name.toUpperCase();
+        return this.http.post(this.resourceUrl,stock).map(res => this.convertResponse(res));
+     }
+
+     update(stock:StockModel):Observable<ResponseWrapper>{
+         stock.code = stock.code.toUpperCase();
+         stock.name = stock.name.toUpperCase();
+        return this.http.put(this.resourceUrl,stock).map(res => this.convertResponse(res));
+     }
+
+     delete(code:string):Observable<Response>{
+        return this.http.delete(this.resourceUrl+"/"+code);
+     }
+
+
+    private convertResponse(res: Response): ResponseWrapper {
+        const jsonResponse = res.json();
+        return new ResponseWrapper(res.headers, jsonResponse, res.status);
+    }
+
+}
